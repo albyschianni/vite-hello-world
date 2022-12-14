7637bb8a39fca78f1b66c074542addd3cbf9db68
albyschianni <alberto.schianni@gmail.com>
2022-12-14 23:12:09 +0100

    update

---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<script >
+export default{
+    name:"AppHero",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+ <div>
+    <img src="../assets/img (134).webp" alt="">
+ </div>
+</template>
+
+<style scoped>
+div {
+    width: 100%;
+}
+img {
+    width: 100%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script >
+export default{
+    name:"AppTitle",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+ <div>
+    <p>LA MIA PRIMA APP VITE</p>
+ </div>
+</template>
+
+<style scoped>
+p {
+    text-align: center;
+}
+</style>
